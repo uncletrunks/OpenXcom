ce583cd6c15595f7b688b754a4f079f45451334d
uncletrunks <uncletrunks@t450s.localdomain>
2020-11-09 00:01:17 -0600

    adding a timestamp to OPK output to avoid a million identical versions running around in the future

--- build_opk.sh
+++ build_opk.sh
@@ -5,4 +5,4 @@ convert res/linux/icons/openxcom_48x48.png  -resize 32x32! build/bin/icon.png
 mksquashfs \
     build/bin/*         \
     build/bin/icon.png                      \
-    default.gcw0.desktop openxcom.opk -noappend
+    default.gcw0.desktop openxcom-$(date '+%s').opk -noappend
